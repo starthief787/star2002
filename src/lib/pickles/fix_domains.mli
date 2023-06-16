31b897ffaf7b2e4cd17fef036ece02812eed8c3c
Danny Willems <be.danny.willems@gmail.com>
2023-06-16 08:20:37 +0200

    Pickles: comment fix_domains

--- src/lib/pickles/fix_domains.mli
+++ src/lib/pickles/fix_domains.mli
@@ -1,3 +1,8 @@
+(** Determines the domain size used for 'wrap proofs'. This can be determined by
+    the fixpoint function provided by {!wrap_domains.f_debug}, but for
+    efficiently this is disabled in production and uses the hard-coded results.
+*)
+
 val domains :
      (module Snarky_backendless.Snark_intf.Run with type field = 'field)
   -> ('a, 'b, 'field) Import.Spec.ETyp.t
